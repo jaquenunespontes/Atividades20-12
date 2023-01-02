f44775b0b96ef1233548d43a380e9e3245877515
Jaqueline Pontes <120497663+jaquenunespontes@users.noreply.github.com>
2023-01-02 19:26:53 -0300

    Atividade 01 - 02-01

---
+++ Atividade1.java
@@ -0,0 +1,27 @@
+package Atividade;
+
+import java.util.Scanner;
+
+public class Atividade1 {
+	public static void main(String[] args) {
+		Scanner leia
+		if (new Scanner(System.in))
+			leia = numero = leia.nextInt();
+		else
+			leia = numero > 0; {
+				System.out.printin("\n numero:" +numero+ - é negativo e par ")"
+				else"
+				
+				System.out.printin ("\n numero:" +numero+ - é positivo e impar")"
+				else 
+				
+				System.our.printin( "\n numero" +numero+ - é negativo e par") "
+				else
+					
+				System.our.printin("\n numero" +numero+ - é negativo e impar)
+				
+						
+					}
+	}
+
+}
